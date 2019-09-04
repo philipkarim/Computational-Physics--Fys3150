49fab565920b840e41485590b745b5a1ffa0fc60
rohullaa <54434823+rohullaa@users.noreply.github.com>
2019-09-04 21:51:40 +0200

    Error, time, output,plot.

--- main.cpp
+++ main.cpp
@@ -10,31 +10,35 @@
 
 using namespace std;
 ofstream ofile;
+
 inline double f(double x) { return 100.0 * exp(-10.0 * x); };
+inline double exact(double x)
+{
+	return 1.0 - (1 - exp(-10)) * x - exp(-10 * x);
+}
 void gauss_generel(int n, double* a, double* b, double* c, double* u, double* s);
 void gauss_special(int n, double* b, double* u, double* s);
+double relativError(double nummerical, double exact);
 
 int main(int argc, char* argv[]) {
 	int ex; int test;
 	string filename;
 
 	if (argc <= 1) {
-		cout << "Bad Usage: you forgot to enter values."  << endl;
+		cout << "Bad Usage: you forgot to enter values." << endl;
 		exit(1);
 	}
 	else {
-		ex = atoi (argv[1]);
+		ex = atoi(argv[1]);
 		test = atoi(argv[2]);
 		filename = argv[3];
 	}
-
 
 	for (int i = 1; i <= ex; i++) {
 		int  n = (int)pow(10.0, i);
 		string fileout = filename;
 		string argument = to_string(i);
 		fileout.append(argument);
-
 
 		double h = 1.0 / n; //stepzise
 		int n1 = n + 1; int n2 = n + 2;
@@ -49,20 +53,20 @@ int main(int argc, char* argv[]) {
 			a[i] = -1.0;
 			b[i] = 2.0;
 			c[i] = -1.0;
-			s[i] = h*h*f(h * i);
+			s[i] = h * h * f(h * i);
 		}
 		u[0] = u[n2] = 0.0; //boundary condition
 		b[n2] = 2.0;
 
-
 		//couting the time
 		clock_t start, finish;
+
 		if (test == 0) {
 			start = clock();
 			gauss_generel(n, a, b, c, u, s);
 			finish = clock();
 			double timeSpent = (double(finish - start) / CLOCKS_PER_SEC);
-			cout << "Time for generel: " << scientific << timeSpent << setprecision(10) << " s med n = " << n<< endl;
+			cout << "Time for generel: " << scientific << timeSpent << setprecision(10) << " s med n = " << n << endl;
 
 		}
 		else if (test == 1) {
@@ -70,20 +74,27 @@ int main(int argc, char* argv[]) {
 			gauss_special(n, b, u, s);
 			finish = clock();
 			double timeSpent = (double(finish - start) / CLOCKS_PER_SEC);
-			cout << "Time for special: " << scientific << timeSpent << setprecision(10) << " s med n = " << n<< endl;
+			cout << "Time for special: " << scientific << timeSpent << setprecision(10) << " s med n = " << n << endl;
 		}
 
-		/*//writing to files
+		//finner den relative feilen:
+		double* error = new double[n2];
+		for (int i = 1; i < n2; i++) {
+			error[i] = relativError(u[i], exact(h * i));
+		}
+		//writing to files
 		ofile.open(fileout);
 		ofile << setiosflags(ios::showpoint | ios::uppercase);
-		for (int i = 1; i < n; i++) {
-			ofile << setw(15) << setprecision(8) << u[i] << endl;
+		for (int z = 1; z < n; z++) {
+			ofile <<  setprecision(8) << left << setw(20) << u[z]
+					<< left << setw(20) << exact(h * z)
+					<< left << setw(20) << error[z] << endl;
 		}
 		ofile.close();
-		delete[] u;*/
+		delete[] u;
+
+
 	}
-
-
 	return 0;
 }
 
@@ -112,4 +123,15 @@ void gauss_special(int n, double* b, double* u, double* s)
 		u[i - 1] = (s[i - 1] + u[i]) / b[i - 1];
 	}
 
+}
+double relativError(double nummerical, double exact){
+	double r;
+	double epslon = 1e-6;
+	if (exact <= epslon) {
+		r = 0;
+	}
+	else {
+		r = abs((nummerical - exact) / (exact));
+	}
+	return r;
 }
