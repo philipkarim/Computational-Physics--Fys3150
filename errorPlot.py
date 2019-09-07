9a05451d3fb0d2ec34ebc4e2599936d73235e684
rohullaa <54434823+rohullaa@users.noreply.github.com>
2019-09-07 15:21:13 +0200

    Update errorPlot.py

--- errorPlot.py
+++ errorPlot.py
@@ -1,7 +1,7 @@
 import pylab as ppl
 import numpy as np
 
-def readFile(readFile): 
+def readFile(readFile):
     x = 0; y = 0;
     file = open(readFile,"r")
     lines = file.readlines()
@@ -13,8 +13,16 @@ def readFile(readFile):
     file.close()
     return x,y;
 
-x = np.zeros(7); 
+x = np.zeros(7);
 y = np.zeros(7);
+
+u = np.zeros(4);
+v = np.zeros(4);
+
+u[0],v[0] = readFile("LU_error_plot1")
+u[1],v[1] = readFile("LU_error_plot2")
+u[2],v[2] = readFile("LU_error_plot3")
+u[3],v[3] = readFile("LU_error_plot4")
 
 x[0],y[0] = readFile("errorFil1")
 x[1],y[1] = readFile("errorFil2")
@@ -25,6 +33,8 @@ x[5],y[5] = readFile("errorFil6")
 x[6],y[6] = readFile("errorFil7")
 
 ppl.plot(x,y, label = "Gauss special")
+ppl.plot(u,v, label = "LU")
+
 ppl.legend()
 ppl.xlabel("log10(h)")
 ppl.ylabel("log10(Maks relativ error)")
